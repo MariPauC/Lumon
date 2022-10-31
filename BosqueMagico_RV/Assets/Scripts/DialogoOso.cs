2d3f7c4c4dd599fb0e3c78a8cf82057ae0e76867
danielsilva14 <80868884+danielsilva14@users.noreply.github.com>
2022-10-31 10:36:47 -0500

    dialogos
    aaaaaa

---
+++ BosqueMagico_RV/Assets/Scripts/DialogoOso.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DialogoOso : MonoBehaviour
+    public AudioSource _audio;
+{
+    void OnTriggerEnter(Collider other)
+    {
+        print("musica on");
+        _audio.Play();
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        print("musica off");
+        _audio.Stop();
+    }
+}
