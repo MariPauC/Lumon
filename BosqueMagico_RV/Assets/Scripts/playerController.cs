ae4503aabc12c5df81daf9c27f71f448d6d9802e
Maria Paula Castrillon <mapaucastrillon@gmail.com>
2022-11-04 18:52:55 -0500

    Movimiento personaje
    Habilitada la opcion para el movimiento completo del personaje con las teclas

--- BosqueMagico_RV/Assets/Scripts/playerController.cs
+++ BosqueMagico_RV/Assets/Scripts/playerController.cs
@@ -4,17 +4,43 @@ using UnityEngine;
 
 public class playerController : MonoBehaviour
 {
-    public float velocidad = 0.2f;
+    public float velocidad = 0.2f; //Velocidad de movimiento
+    public bool movFrontal = false; 
+    public bool movLateral = true; 
+    
     public Rigidbody rb;
 
     void Start(){
         rb = GetComponent<Rigidbody> ();
     }
 
-    void Update(){
-        transform.Translate(Vector3.forward * velocidad * Time.deltaTime);
-        float h = Input.GetAxis("Horizontal");
+    void Update(){ 
+        float frontal = Input.GetAxis("Vertical");
+        float lados = Input.GetAxis("Horizontal");
 
+        if(movFrontal){
+            movimientoFrontal(frontal);
+        }
+        else{
+            transform.Translate(Vector3.forward * velocidad * Time.deltaTime);
+        }
+        
+        if(movLateral){
+            movimientoLados(lados);
+        }
+        
+    }
+
+    private void movimientoFrontal(float h){
+        if(h > 0){
+            transform.Translate(Vector3.forward * velocidad * Time.deltaTime);
+        }
+        if(h < 0){
+            transform.Translate(Vector3.back * velocidad * Time.deltaTime);
+        }
+    }
+
+    private void movimientoLados(float h){
         if(h > 0){
             transform.Translate(Vector3.right * velocidad * Time.deltaTime);
         }
