705d10e6f785b79d5e95a052a9e8cd30c080af80
michaelsrr <80532798+michaelsrr@users.noreply.github.com>
2022-11-10 22:52:03 -0500

    Sonido y juego frutas
    aaaaaaaaaaaa

---
+++ BosqueMagico_RV/Assets/Scripts/VidaFruta.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VidaFruta : MonoBehaviour
+{
+    public int AunmVida;
+    public GameObject Player;
+    public AudioSource Emite;
+    public AudioClip elSonido;
+    public float volimen = 1f;
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            Player.GetComponent<DatosJugador>().vidaPlayer += AunmVida; 
+            Emite.PlayOneShot(elSonido, volimen);
+            Destroy(gameObject, 0.2f);
+        }
+    }
+}
